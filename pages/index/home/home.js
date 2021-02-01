79ee81fada9be24966f8a561b259e11fc5e6cd13
Kolento <515835543@qq.com>
2021-02-01 17:38:15 +0800

    首页开发
    Signed-off-by: Kolento <515835543@qq.com>

--- pages/index/home/home.js
+++ pages/index/home/home.js
@@ -6,11 +6,41 @@ Page({
    */
   data: {
     keywords:'',
-    value:''
+    // 轮播图
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500
   },
 
   onSearch(e){
     console.log('e',e);
+  },
+
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
   },
 
   /**
