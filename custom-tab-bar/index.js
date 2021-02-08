19b1691a061ef41336c7dc7fa6b115b67fb35240
LXQ <515835543@qq.com>
2021-02-08 21:53:02 +0800

    修复底部组件问题
    Signed-off-by: LXQ <515835543@qq.com>

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,58 @@
+// components/bottom.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    active:0,
+    list: [
+      {
+        "url": "/pages/home/home",
+        "icon": "home-o",
+        "text": "首页"
+      },
+      {
+        "url": "/pages/anime/anime",
+        "icon": "flower-o",
+        "text": "番剧"
+      },
+      {
+        "url": "/pages/discover/discover",
+        "icon": "shop-collect-o",
+        "text": "发现"
+      },
+      {
+        "url": "/pages/my/my",
+        "icon": "smile-o",
+        "text": "我的"
+      }
+    ]
+  },
+
+
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+		onChange(event) {
+			this.setData({ active: event.detail });
+			wx.switchTab({
+				url: this.data.list[event.detail].url
+			});
+		},
+    init() {
+			const page = getCurrentPages().pop();
+			this.setData({
+				active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+			});
+		}
+  }
+})
